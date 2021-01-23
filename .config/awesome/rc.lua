582bb394420d32b5b3d32749230dbb50f6cdf39f
bbastanza <b.bastanza@gmail.com>
2021-01-22 21:08:42 -0800

    add new layouts

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -93,18 +93,18 @@ local scrlocker    = "slock"
 awful.util.terminal = terminal
 awful.util.tagnames = { "  ", " ‹∕› ", " 球 ", "  ", "  ", " ♫ ", "  " }
 awful.layout.layouts = {
+    awful.layout.suit.corner.nw,
+    --awful.layout.suit.corner.ne,
+    --awful.layout.suit.corner.sw,
+    --awful.layout.suit.corner.se,
     awful.layout.suit.tile,
     --awful.layout.suit.tile.left,
-    --awful.layout.suit.tile.bottom,
+    awful.layout.suit.tile.bottom,
     --awful.layout.suit.tile.top,
     --awful.layout.suit.fair,
     --awful.layout.suit.fair.horizontal,
     --awful.layout.suit.spiral,
     --awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.corner.nw,
-    --awful.layout.suit.corner.ne,
-    --awful.layout.suit.corner.sw,
-    --awful.layout.suit.corner.se,
     awful.layout.suit.max,
     --awful.layout.suit.max.fullscreen,
     --awful.layout.suit.magnifier,
